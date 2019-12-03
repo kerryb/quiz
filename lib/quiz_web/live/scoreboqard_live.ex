369a70bba354252c707740195c58b2aaedfe6927
Kerry Buckley <kerryjbuckley@gmail.com>
2019-12-03 11:08:46 +0000

    Handle buzzer keypresses

--- lib/quiz_web/live/scoreboqard_live.ex
+++ lib/quiz_web/live/scoreboqard_live.ex
@@ -10,10 +10,23 @@ defmodule QuizWeb.ScoreboardLive do
   end
 
   defp initial_teams do
-    [
-      %{id: 1, name: "Team one", score: 0, buzzed?: false},
-      %{id: 2, name: "Team two", score: 0, buzzed?: false},
-      %{id: 3, name: "Team three", score: 0, buzzed?: false}
-    ]
+    %{
+      "1" => %{name: "Team one", score: 0, buzzed?: false},
+      "2" => %{name: "Team two", score: 0, buzzed?: false},
+      "3" => %{name: "Team three", score: 0, buzzed?: false}
+    }
+  end
+
+  def handle_event("keydown", %{"key" => key}, socket) when key in ~w(1 2 3 4) do
+    teams = socket.assigns.teams |> buzz(key)
+    {:noreply, socket |> assign(:teams, teams) |> IO.inspect()}
+  end
+
+  defp buzz(teams, key) do
+    teams |> put_in([key, :buzzed?], true)
+  end
+
+  def handle_event(_event, _args, socket) do
+    {:noreply, socket}
   end
 end
