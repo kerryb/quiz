231048bf23108d80779285b453556872689ec9bc
Kerry Buckley <kerryjbuckley@gmail.com>
2019-12-03 14:23:55 +0000

    Only allow one buzz at once (cancel with escape key)

--- lib/quiz_web/live/scoreboqard_live.ex
+++ lib/quiz_web/live/scoreboqard_live.ex
@@ -6,7 +6,7 @@ defmodule QuizWeb.ScoreboardLive do
   end
 
   def mount(_, socket) do
-    {:ok, assign(socket, teams: initial_teams())}
+    {:ok, assign(socket, teams: initial_teams(), state: :question)}
   end
 
   defp initial_teams do
@@ -17,13 +17,23 @@ defmodule QuizWeb.ScoreboardLive do
     }
   end
 
-  def handle_event("keydown", %{"key" => key}, socket) when key in ~w(1 2 3 4) do
+  def handle_event("keydown", %{"key" => key}, %{assigns: %{state: :question}} = socket)
+      when key in ~w(1 2 3 4) do
     teams = socket.assigns.teams |> buzz(key)
-    {:noreply, socket |> assign(:teams, teams) |> IO.inspect()}
+    {:noreply, socket |> assign(teams: teams, state: :answer)}
   end
 
   defp buzz(teams, key) do
     teams |> put_in([key, :buzzed?], true)
+  end
+
+  def handle_event("keydown", %{"key" => "Escape"}, %{assigns: %{state: :answer}} = socket) do
+    teams = socket.assigns.teams |> Enum.into(%{}, &clear_buzzed/1)
+    {:noreply, socket |> assign(teams: teams, state: :question)}
+  end
+
+  defp clear_buzzed({id, team}) do
+    {id, %{team | buzzed?: false}}
   end
 
   def handle_event(_event, _args, socket) do
