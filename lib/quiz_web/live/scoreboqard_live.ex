ec14eaaf0c5d4c521c9dcfbc307b79f900c18848
Kerry Buckley <kerryjbuckley@gmail.com>
2019-12-03 10:34:10 +0000

    Set up teams in live view

--- lib/quiz_web/live/scoreboqard_live.ex
+++ lib/quiz_web/live/scoreboqard_live.ex
@@ -4,4 +4,16 @@ defmodule QuizWeb.ScoreboardLive do
   def render(assigns) do
     Phoenix.View.render(QuizWeb.ScoreboardView, "index.html", assigns)
   end
+
+  def mount(_, socket) do
+    {:ok, assign(socket, teams: initial_teams())}
+  end
+
+  defp initial_teams do
+    [
+      %{id: 1, name: "Team one", score: 0, buzzed?: false},
+      %{id: 2, name: "Team two", score: 0, buzzed?: false},
+      %{id: 3, name: "Team three", score: 0, buzzed?: false}
+    ]
+  end
 end
